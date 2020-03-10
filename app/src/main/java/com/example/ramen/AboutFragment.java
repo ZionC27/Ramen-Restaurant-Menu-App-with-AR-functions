e0926e31bfbd317154cbf694562af95d419477cb
aburke-ship <burkeadam@icloud.com>
2020-03-10 15:46:08 -0700

    Added a description and picture on the about page.... Commented out a portion of code in the about page -- its purpose eventually will be to link social media to the page.

--- app/src/main/java/com/example/ramen/AboutFragment.java
+++ app/src/main/java/com/example/ramen/AboutFragment.java
@@ -18,7 +18,7 @@ import androidx.fragment.app.Fragment;
 public class AboutFragment extends Fragment
 {
 
-    private Button btnChange;
+    private Button button;
     private TextView textView;
 
     @Nullable
@@ -26,11 +26,30 @@ public class AboutFragment extends Fragment
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
     {
 
+        //link to website that build all of the button code:
+        //https://www.c-sharpcorner.com/article/sociamedia-link-share-in-android-using-androi/
+
+       /* button=(Button) getView().findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("text/plain");
+                String shareBody = "Your body is here";
+                String shareSub = "Your subject";
+                intent.putExtra(Intent.EXTRA_SUBJECT, shareBody);
+                intent.putExtra(Intent.EXTRA_TEXT, shareBody);
+                startActivity(Intent.createChooser(intent, "share using"));
+
+            }
+        });
+
+*/
         //orginal code
         return inflater.inflate(R.layout.fragment_about, container, false);
 
 
     }//end onCreateView
-
 
 }//end class
