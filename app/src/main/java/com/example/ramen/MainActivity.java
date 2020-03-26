657dbafdf89d9be6305b7cc28f7408ccde5c3054
jordan <jordanabel43@gmail.com>
2020-03-25 17:08:37 -0700

    ActionBar

--- app/src/main/java/com/example/ramen/MainActivity.java
+++ app/src/main/java/com/example/ramen/MainActivity.java
@@ -26,6 +26,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+
         drawer = findViewById(R.id.drawer_layout);
 
         NavigationView navigationView = findViewById(R.id.nav_view);
